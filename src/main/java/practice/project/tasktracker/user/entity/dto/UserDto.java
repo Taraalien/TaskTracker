578c288816526cdd23a14a46bcf5fe7590e8b474
Bahmani <tara_geek@protonmail.com>
2024-08-27 15:27:07 +0330

    add new model called user and edit TaskController

---
+++ src/main/java/practice/project/tasktracker/user/entity/dto/UserDto.java
@@ -0,0 +1,10 @@
+package practice.project.tasktracker.user.entity.dto;
+
+
+public record UserDto(
+
+                     String userName,
+                     String password,
+                     String gmail)
+        {
+}
